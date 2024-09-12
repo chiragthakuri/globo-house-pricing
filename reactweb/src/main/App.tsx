5a2a55b1979e6ac63cc2f156af5ae4b51448066d
Chirag <chiragbikram@gmail.com>
2024-09-11 17:41:25 -0700

    Removed reactweb submodule and kept as a regular directory

---
+++ reactweb/src/main/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import HouseList from '../house/HouseList';
+
+function App() {
+  return (
+          <div className="container">
+            <Header subtitle="Providing housing all over the world" />
+            <HouseList />
+          </div>
+  );
+}
+
+export default App;
