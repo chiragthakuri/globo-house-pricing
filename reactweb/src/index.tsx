4df17b0491503d127a6c3699b682501d5a434aa0
Chirag <chiragbikram@gmail.com>
2024-09-11 23:11:17 -0700

    creating an api end point for house details

--- reactweb/src/index.tsx
+++ reactweb/src/index.tsx
@@ -4,13 +4,19 @@ import './index.css';
 import App from './main/App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
+import {QueryClient, QueryClientProvider} from 'react-query';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
