688134e104c1230df85a45a37a999c5f67351d6a
Chirag <chiragbikram@gmail.com>
2024-09-11 17:10:11 -0700

    using custom hooks to display data

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d231cbdd0bc2d1c3acfd6c19ff134a8f5ce6515")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2429ecfd42451eb29376cd1dde4d63c96950b2de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
