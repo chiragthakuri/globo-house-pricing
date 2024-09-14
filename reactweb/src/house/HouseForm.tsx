b7cfc4e0ddf5b815cc53cd34aaf36783eef4d8cb
Chirag <chiragbikram@gmail.com>
2024-09-13 19:04:01 -0700

    file upload feature added

--- reactweb/src/house/HouseForm.tsx
+++ reactweb/src/house/HouseForm.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { House } from "../types/house";
+import toBase64 from "../toBase64";
 
 type Args = {
     house: House;
@@ -14,6 +15,18 @@ const HouseForm = ({house, submitted}: Args) => {
                 e.preventDefault();
                 submitted(houseState);
             }
+    
+  const onFileSelected = async (
+    e: React.ChangeEvent<HTMLInputElement>
+        ): Promise<void> => {
+            e.preventDefault();
+            e.target.files &&
+              e.target.files[0] &&
+              setHouseState({
+                ...houseState,
+                photo: await toBase64(e.target.files[0]),
+               });
+          };
 
     return(
     <form className="mt-2">
@@ -63,6 +76,19 @@ const HouseForm = ({house, submitted}: Args) => {
             setHouseState({ ...houseState, price: parseInt(e.target.value) })
           }
         />
+      </div>
+
+      <div className="form-group mt-2">
+        <label htmlFor="image">Image</label>
+        <input
+          id="image"
+          type="file"
+          className="form-control"
+          onChange={onFileSelected}
+        />
+      </div>
+      <div className="mt-2">
+        <img src={houseState.photo}></img>
       </div>
       
       <button
